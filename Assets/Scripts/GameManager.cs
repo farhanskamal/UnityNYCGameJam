ec6e558fe6a3b4aa6c574369cfbd7a8a7409d496
UltraZavk2Laptop <ultrazavk2@gmail.com>
2026-09-20 13:21:15 -0400

    Added some changes, descriptions still incomplete.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> NPCs = new List<GameObject>();
 
+    public GameObject Hunted;
+
     public GameObject[] spawnlocations;
     public GameObject NPCPref;
     // Start is called before the first frame update
@@ -20,6 +22,10 @@ public class GameManager : MonoBehaviour
         if(NPCs.Count != 20)
         {
             NPCs.Add(Instantiate(NPCPref, spawnlocations[Random.Range(0, spawnlocations.Length)].transform.position, Quaternion.identity));
+        }
+        if(NPCs.Count == 20 && Hunted == null)
+        {
+            Hunted = NPCs[Random.Range(0, NPCs.Count)];
         }
     }
 }
