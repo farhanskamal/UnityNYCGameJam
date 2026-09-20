42414199c6f04bf5932bedf3eb8bfbb9d69e7471
BrandonC-EGD <brandon.m.covarrubias@gmail.com>
2026-09-19 20:39:55 -0400

    font

--- Assets/NPC.cs
+++ Assets/NPC.cs
@@ -17,7 +17,7 @@ public class NPC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        NPCAssets = GameObject.Find("NPCAssetsHolder").GetComponent<NPCAssetHolder>();
     }
 
     // Update is called once per frame
