0283548ccdf0d4760f9ec4ef0a5b585b1022f057
UltraZavk2Laptop <ultrazavk2@gmail.com>
2026-09-20 16:48:42 -0400

    Completed Build, finalized win/lose screen

--- Assets/Scripts/AddAtEnd.cs
+++ Assets/Scripts/AddAtEnd.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AddAtEnd : MonoBehaviour
 {
+    public GameObject dialogue;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class AddAtEnd : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(dialogue.GetComponent<Dialogue>().index == dialogue.GetComponent<Dialogue>().lines.Length-1)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, .1f);
+        }
     }
 }
